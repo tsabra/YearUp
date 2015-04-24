8e3d396edffa051c287247fbb665b8e2055e1868
tsabra116 <tsabra@atl.yearup.org>
2015-04-24 16:53:31 -0400

    This is my first project for Java Class.

---
+++ Project1/src/Intro.java
@@ -0,0 +1,11 @@
+
+public class Intro {
+	public static void main(String[] args) {
+		System.out.println("Hello! My name is Taha Sabra. Welcome to Java class.");
+		/*
+		 * 1)I removed the end bracket and received a Syntax error to insert ")"
+		 * 2)I removed the end quote and received a string error to close the quote
+		 * 3)There are no more compilation errors.
+		 */
+	}
+}
